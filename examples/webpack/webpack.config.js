091501b6e9a3bd53a4ea5276dfe76bb8f619ee0b
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-04-27 18:48:30 -0700

    Rewrite Webpack loader

--- examples/webpack/webpack.config.js
+++ examples/webpack/webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const localIdentName = process.env.NODE_ENV === 'production' ? 'DSS-[hash:base32]' : '[name]-[local]-[hash:base32:5]'
+const mode = process.env.NODE_ENV || 'development'
+
 const config = {
+  mode,
   entry: path.resolve('./src/index.js'),
   output: {
     path: path.resolve("./dist"),
@@ -11,16 +16,26 @@ const config = {
     rules: [
       {
         test: /\.css$/,
-        loader: require.resolve('dss/webpack'),
-        options: {
-          bundleFilename: path.join(path.resolve("./dist"), 'index.css')
-        }
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: require.resolve('dss/webpack'),
+            query: {
+              localIdentName
+            }
+          }
+        ],
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve('./src/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "index.css",
     }),
   ]
 }
--- src/webpack.js
+++ src/webpack.js
@@ -1,41 +1,29 @@
-const path = require('path')
 const loaderUtils = require('loader-utils')
 const dss = require('./index')
 
-const BUNDLE_MARKER = '/*___DSS_BUNDLE___*/'
-let loaded = false
+const BANNER = '/* DSS file */'
+
 module.exports = function(content) {
+  if (this.cacheable) this.cacheable()
   const callback = this.async()
   const options = loaderUtils.getOptions(this) || {}
-
-  // Don't process the bundle file
-  if (
-    options.processBundleWithNextLoaders &&
-    content.indexOf(BUNDLE_MARKER) !== -1
-  ) {
-    callback(content)
-    return
+  let readableClass
+  if (typeof options.localIdentName === 'string') {
+    readableClass = localName =>
+      loaderUtils
+        .interpolateName(this, options.localIdentName, { content })
+        .split('[local]')
+        .join(localName)
   }
 
-  let bundleFilename = options.bundleFilename
-  if (typeof bundleFilename !== 'string') {
-    bundleFilename = 'index.css'
-  }
-
-  if (this.cacheable) this.cacheable()
-
-  dss(content)
+  dss(content, { readableClass })
     .then(compiled => {
-      this.emitFile(
-        path.basename(bundleFilename),
-        `${BUNDLE_MARKER}${dss.css()}`
-      )
-
-      if (options.processBundleWithNextLoaders && !loaded) {
-        loaded = true
-        this.loadModule(bundleFilename, () => {})
-      }
-      callback(`module.exports = ${JSON.stringify(compiled)}`)
+      const moduleExports = [
+        BANNER,
+        `exports = module.exports = [[module.id, "${dss.flush()}", ""]];`,
+        `exports.locals = ${JSON.stringify(compiled)}`
+      ].join('\n')
+      callback(null, moduleExports)
     })
     .catch(callback)
 }
