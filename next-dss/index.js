486bd5cbfd9a181f2cc7fdee755f13299dd417c9
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-05-20 12:26:36 +0200

    Create next-dss

---
+++ next-dss/index.js
@@ -0,0 +1,74 @@
+const DSSWebpackPlugin = require('@dss/webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const cssLoaderConfig = require('@zeit/next-css/css-loader-config')
+const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config')
+const escapeStringRegexp = require('escape-string-regexp')
+
+module.exports = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.defaultLoaders) {
+        throw new Error(
+          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+        )
+      }
+
+      const { dev, isServer } = options
+      const { dssLoaderOptions } = nextConfig
+      // Support the user providing their own instance of ExtractTextPlugin.
+      // If extractCSSPlugin is not defined we pass the same instance of ExtractTextPlugin to all css related modules
+      // So that they compile to the same file in production
+      let extractCSSPlugin = nextConfig.extractCSSPlugin || options.extractCSSPlugin
+
+      const bundleName = dssLoaderOptions.filename || 'static/style.css'
+
+      if (!extractCSSPlugin) {
+        extractCSSPlugin = new ExtractTextPlugin({
+          filename: bundleName
+        })
+        config.plugins.push(extractCSSPlugin)
+        options.extractCSSPlugin = extractCSSPlugin
+        if (!isServer) {
+          config = commonsChunkConfig(config, /\.css$/)
+        }
+      }
+
+      options.defaultLoaders.css = cssLoaderConfig(config, extractCSSPlugin, {
+        cssModules: true,
+        cssLoaderOptions: {},
+        dev,
+        isServer: false
+      }).map(loader => {
+        // Replace css-loader with the dss-loader
+
+        if (!/^css-loader/.test(loader.loader)) {
+          return loader
+        }
+
+        return {
+          loader: DSSWebpackPlugin.loader,
+          query: {
+            localIdentName: dssLoaderOptions.localIdentName
+          }
+        }
+      })
+
+      config.module.rules.push({
+        test: /\.css$/,
+        use: options.defaultLoaders.css
+      })
+
+      config.plugins.push(
+        new DSSWebpackPlugin({
+          test: new RegExp(escapeStringRegexp(bundleName))
+        })
+      )
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    }
+  })
+}
--- website/next.config.js
+++ website/next.config.js
@@ -1,80 +1,16 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const DSSWebpackPlugin = require('@dss/webpack')
+const withDSS = require('@dss/next-dss')
+const withMDX = require('@zeit/next-mdx')({
+  extension: /\.mdx?$/
+})
 
 const localIdentName =
   process.env.NODE_ENV === 'production' ? 'DSS-[hash:base32]' : '[name]-[local]--[hash:base32:5]'
 
-module.exports = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  webpack: (config, { defaultLoaders, isServer }) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: [
-        defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader'
-        }
-      ]
-    })
-
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        use: [
-          {
-            loader: DSSWebpackPlugin.loader,
-            query: {
-              localIdentName
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                require('postcss-easy-import')(),
-                require('postcss-simple-vars')({ variables: () => require('./theme') })
-              ],
-              sourceMap: false
-            }
-          }
-        ]
-      })
-    })
-
-    config.plugins.push(new ExtractTextPlugin({ filename: 'static/index.css' }))
-
-    config.plugins.push(
-      new DSSWebpackPlugin({
-        test: /static\/index\.css$/
-      })
-    )
-
-    if (!isServer) {
-      // Extend the default CommonsChunkPlugin config
-      // so that Next.js can pick up all the CSS files.
-      //
-      // @TODO make a PR to zeit/next-css to support DSS and remove all this boilerplate.
-      config.plugins = config.plugins.map(plugin => {
-        if (
-          plugin.constructor.name === 'CommonsChunkPlugin' &&
-          typeof plugin.minChunks !== 'undefined'
-        ) {
-          const defaultMinChunks = plugin.minChunks
-          plugin.minChunks = (module, count) => {
-            // Move all styles to commons chunk so they can be extracted to a single file
-            if (module.resource && module.resource.match(/\.css$/)) {
-              return true
-            }
-            // Use default minChunks for non-style modules
-            return typeof defaultMinChunks === 'function'
-              ? defaultMinChunks(module, count)
-              : count >= defaultMinChunks
-          }
-        }
-        return plugin
-      })
+module.exports = withMDX(
+  withDSS({
+    dssLoaderOptions: {
+      localIdentName,
+      filename: 'static/index.css'
     }
-
-    return config
-  }
-}
+  })
+)
