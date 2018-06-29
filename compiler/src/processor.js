24b50a3dc5e5c8c29429011dcedbf0b47a74a0ac
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-06-29 11:32:57 +0200

    fix linting

--- compiler/src/processor.js
+++ compiler/src/processor.js
@@ -12,10 +12,12 @@ const processor = postcss([
 ])
 module.exports = (css, opts = { from: undefined }) => processor.process(css, opts)
 
-const optimzr = require('postcss')([
+const optimzr = postcss([
+  /* eslint-disable import/order */
   require('postcss-discard-duplicates'),
   require('autoprefixer'),
   require('./plugins/sort-at-rules'),
+  /* eslint-enable import/order */
 ])
 
 module.exports.optimizer = (css, opts = { from: undefined, to: undefined }) =>
