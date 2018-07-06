e9f0e7a63b0ba98530e00297fa288533c5dd6cb4
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2018-07-06 20:03:36 +0300

    website: add meta tags

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -6,8 +6,43 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta charset="utf-8" />
+          <title>Deterministic Style Sheets</title>
+
           <style>{`* { box-sizing: border-box; }`}</style>
           <link rel="stylesheet" href="/_next/static/index.css" />
+
+          <meta
+            name="description"
+            content="DSS is like CSS Modules but styles are compiled to atomic CSS classes which thanks to a helper resolve deterministically."
+          />
+          <meta name="image" content="/_next/static/dss.png" />
+
+          <meta itemprop="name" content="Deterministic Style Sheets" />
+          <meta
+            itemprop="description"
+            content="DSS is like CSS Modules but styles are compiled to atomic CSS classes which thanks to a helper resolve deterministically."
+          />
+          <meta itemprop="image" content="/_next/static/dss.png" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content="Deterministic Style Sheets" />
+          <meta
+            name="twitter:description"
+            content="DSS is like CSS Modules but styles are compiled to atomic CSS classes which thanks to a helper resolve deterministically."
+          />
+          <meta name="twitter:site" content="@giuseppegurgone" />
+          <meta name="twitter:image:src" content="/_next/static/dss.png" />
+
+          <meta name="og:title" content="Deterministic Style Sheets" />
+          <meta
+            name="og:description"
+            content="DSS is like CSS Modules but styles are compiled to atomic CSS classes which thanks to a helper resolve deterministically."
+          />
+          <meta name="og:image" content="/_next/static/dss.png" />
+          <meta name="og:site_name" content="Deterministic Style Sheets" />
+          <meta name="og:locale" content="en_US" />
+          <meta name="og:type" content="website" />
         </Head>
         <Body>
           <Main />
